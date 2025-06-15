c33910771481f59572f9a78dcf13fa67b9a4f3bc
rahulnahak <rahul.nahak1995@gmail.com>
2025-06-15 14:43:21 +0200

    re-design business logic to generate rental slip

--- src/main/java/com/etraveli/movierental/controller/GenerateSlipController.java
+++ src/main/java/com/etraveli/movierental/controller/GenerateSlipController.java
@@ -2,6 +2,7 @@ package com.etraveli.movierental.controller;
 
 import com.etraveli.movierental.models.CustomerDetails;
 import com.etraveli.movierental.services.RentalInfo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/movie-rental")
 public class GenerateSlipController {
+    @Autowired
+    RentalInfo rentalInfo;
     @GetMapping ("/generate-slip")
     public String getMovieRentalSlip(@RequestBody CustomerDetails customerDetails){
-        return new RentalInfo().statement(customerDetails);
+        return rentalInfo.statement(customerDetails);
     }
 }
--- src/main/java/com/etraveli/movierental/models/CustomerDetails.java
+++ src/main/java/com/etraveli/movierental/models/CustomerDetails.java
@@ -1,21 +1,12 @@
 package com.etraveli.movierental.models;
 
-import java.util.List;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.List;
+@Data
+@NoArgsConstructor
 public class CustomerDetails {
     private String name;
     private List<MovieRental> rentals;
-
-    public CustomerDetails(String name, List<MovieRental> rentals) {
-        this.name = name;
-        this.rentals = rentals;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public List<MovieRental> getRentals() {
-        return rentals;
-    }
 }
